cded6f01d8652b0540e5c93520475037bb258ba6
h-ellen <123069394+h-ellen@users.noreply.github.com>
2023-05-10 13:58:23 +0300

    Update initialising.c

--- initialising.c
+++ initialising.c
@@ -1,37 +1,39 @@
 #include "shel.h"
+
 /**
  * initalize - function used to initialize our shell.
  * Return: Always zero
  */
+
 void initalize()
 {
-	GBSH_PID = getpid();
-	GBSH_IS_INTERACTIVE = isatty(STDIN_FILENO);
-		if (GBSH_IS_INTERACTIVE)
+	PID_SH = getpid();
+	INTERACTIVE_SH = isatty(STDIN_FILENO);
+		if (INTERACTIVE_SH)
 		{
-			while (tcgetpgrp(STDIN_FILENO) != (GBSH_PGID = getpgrp()))
-					kill(GBSH_PID, SIGTTIN);
+			while (tcgetpgrp(STDIN_FILENO) != (PGID_SH = getpgrp()))
+					kill(PID_SH, SIGTTIN);
 
-			act_child.sa_handler = signalHandler_child;
-			act_int.sa_handler = signalHandler_int;
+			act_child.sa_handler = _child_handler;
+			act_int.sa_handler = _int_handler;
 
 			sigaction(SIGCHLD, &act_child, 0);
 			sigaction(SIGINT, &act_int, 0);
 	
-			setpgid(GBSH_PID, GBSH_PID);
+			setpgid(PID_SH, PID_SH);
 
-			GBSH_PGID = getpgrp();
-			if (GBSH_PID != GBSH_PGID)
+			PGID_SH = getpgrp();
+			if (PID_SH != PGID_SH)
 			{
 					printf("Error, the shell is not process group leader");
 					exit(EXIT_FAILURE);
 			}
 			/*Grab control of the terminal*/
-			tcsetpgrp(STDIN_FILENO, GBSH_PGID);
+			tcsetpgrp(STDIN_FILENO, PGID_SH);
 
-			tcgetattr(STDIN_FILENO, &GBSH_TMODES);
+			tcgetattr(STDIN_FILENO, &MOD_SH);
 
-			currentDirectory = (char*) calloc(1024, sizeof(char));
+			current_dir = (char*) calloc(1024, sizeof(char));
         }
 	else
 	{
@@ -41,36 +43,28 @@ void initalize()
 }
 
 /**
- * SIGNAL HANDLERS
- */
-
-/**
- * signalHandler_child - signal handler for SIGCHLD
+ * _child_handler - signal handler for SIGCHLD
  * Return: always zero
  */
-void signalHandler_child(int p)
+void _child_handler(int p)
 {
-	/**
-	 * Wait for all dead processes.
-	 * We use a non-blocking call (WNOHANG) to be sure this signal handler will not
-	 * block if a child was cleaned up in another part of the program.
-	 */
-	while (waitpid(-1, NULL, WNOHANG) > 0) {
+	while (waitpid(-1, NULL, WNOHANG) > 0)
+	{
 	}
 	printf("\n");
 }
 
 /**
- * signalHandler_int - Signal handler for SIGINT
+ * _int_handler - Signal handler for SIGINT
  * Return: always zero
  */
-void signalHandler_int(int p)
+void _int_handler(int p)
 {
 	/*We send a SIGTERM signal to the child process*/
 	if (kill(pid,SIGTERM) == 0)
 	{
 		printf("\nProcess %d received a SIGINT signal\n",pid);
-		no_reprint_prmpt = 1;			
+		no_prompt = 1;			
 	}
 	else
 	{

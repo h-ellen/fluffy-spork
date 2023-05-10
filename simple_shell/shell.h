e0d3c8eb527db50f7441c496e0ddfeb4151d444e
h-ellen <123069394+h-ellen@users.noreply.github.com>
2023-05-10 16:34:37 +0300

    Update shell.h

--- simple_shell/shell.h
+++ simple_shell/shell.h
@@ -21,9 +21,9 @@
 static pid_t __attribute__ ((unused)) PID_SH;
 static pid_t __attribute__ ((unused)) PGID_SH;
 static int __attribute__ ((unused)) INTERACTIVE_SH;
-static struct termios MOD_SH;
-static char* current_dir;
-extern char** environ;
+static struct __attribute__ ((unused)) termios MOD_SH;
+static char* __attribute__ ((unused)) current_dir;
+extern char** __attribute__ ((unused)) environ;
 struct sigaction act_child;
 struct sigaction act_int;
 int no_prompt;

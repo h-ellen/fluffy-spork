c0e420be2196e6b660422d1d444455e41d3fe3c7
h-ellen <123069394+h-ellen@users.noreply.github.com>
2023-05-10 10:49:23 +0300

    Update and rename util.h to shel.h

--- shel.h
+++ shel.h
@@ -1,3 +1,6 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,8 +9,7 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include <termios.h>
-#include "util.h"
+#include <termios.>
 
 #define LIMIT 256
 #define MAXLINE 1024
@@ -15,30 +17,21 @@
 #define TRUE 1
 #define FALSE !TRUE
 
-// Shell pid, pgid, terminal modes
+/*Shell pid, pgid, terminal modes*/
 static pid_t GBSH_PID;
 static pid_t GBSH_PGID;
 static int GBSH_IS_INTERACTIVE;
 static struct termios GBSH_TMODES;
-
 static char* currentDirectory;
 extern char** environ;
-
 struct sigaction act_child;
 struct sigaction act_int;
-
 int no_reprint_prmpt;
-
 pid_t pid;
 
-
-/**
- * SIGNAL HANDLERS
- */
-// signal handler for SIGCHLD */
+/*signal handlers*/
 void signalHandler_child(int p);
-// signal handler for SIGINT
 void signalHandler_int(int p);
 
-
 int changeDirectory(char * args[]);
+#endif /*SHELL_H*/

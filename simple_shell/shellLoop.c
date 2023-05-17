507bdcea587b8cc491ede7d2ca2290d40451d253
h-ellen <123069394+h-ellen@users.noreply.github.com>
2023-05-17 16:51:39 +0300

    Update shellLoop.c

--- simple_shell/shellLoop.c
+++ simple_shell/shellLoop.c
@@ -4,44 +4,22 @@
 * main - function method of our shell
 * Return: alwayz zero
 */
-int main(int argc __attribute__((unused)), char ** envp)
-{
-	char line[LINEMAX];
-	char *tokens[LIM];
-	int tokenNum;
 
-	no_prompt = 0;
-  pid = -10;
-	
-	initalize();
+int main() {
+    char command[100];
 
-	environ = envp;
+    while (1) {
+        printf("user$ ");
+        fgets(command, sizeof(command), stdin);
 
-	setenv("shell",getcwd(current_dir, 1024),1);
+        /*Check for the exit command*/
+        if (strcmp(command, "exit\n") == 0) {
+            exit(0);
+        }
 
-	while(TRUE)
-	{
-		/*We print the shell prompt if necessary*/
-		if (no_prompt == 0) shellPrompt();
-		no_prompt = 0;
-		
-		/*We empty the line buffer*/
-		memset ( line, '\0', LINEMAX );
+        /*Execute the command*/
+        system(command);
+    }
 
-		/*We wait for user input*/
-		fgets(line, LINEMAX, stdin);
-	
-		/*If nothing is written, the loop is executed again*/
-		if((tokens[0] = strtok(line," \n\t")) == NULL) continue;
-		
-		/*We read all the tokens of the input and pass it to our*/
-		/*commandHandler as the argument*/
-		tokenNum = 1;
-		while((tokens[tokenNum] = strtok(NULL, " \n\t")) != NULL) tokenNum++;
-		
-		commands(tokens);
-		
-	}          
-
-	exit(0);
+    return 0;
 }

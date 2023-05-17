8663f3f97693c40cade30a98a8a67c0f3c1ab10d
h-ellen <123069394+h-ellen@users.noreply.github.com>
2023-05-17 15:29:33 +0300

    Update not-interactive.c

--- simple_shell/not-interactive.c
+++ simple_shell/not-interactive.c
@@ -26,5 +26,134 @@ void not_interactive(void)
  } while (status == -1);
 }
 
-/*
- *read_stream - 
+/**
+ *read_stream - read a line from the stream
+ *
+ *Return: pointer to read string
+ */
+char *read_stream(void)
+{
+ int bufsize = 1024;
+ int i = 0;
+ char *line = malloc(sizeof(char) * bufsize);
+ int character;
+
+ if (line == NULL)
+ {
+  fprintf(stderr, "allocation error in read_stream");
+  exit(EXIT_FAILURE);
+ }
+ while (1)
+ {
+  character = getchar(); /* read first char from stream */
+  if (character == EOF)
+  {
+   free(line);
+   exit(EXIT_SUCCESS);
+  }
+  else if (character == '\n')
+  {
+   line[i] = '\0';
+   return (line);
+  }
+  else
+  {
+   line[i] = character;
+  }
+  i++;
+  if (i >= bufsize)
+  {
+   bufsize += bufsize;
+   line = realloc(line, bufsize);
+   if (line == NULL)
+   {
+    fprintf(stderr, "reallocation error in read_stream");
+    exit(EXIT_FAILURE);
+   }
+  }
+ }
+}
+
+/**
+ * split_line - split a string into multiple strings
+ * @line: string to be splited
+ *
+ * Return: pointer that points to the new array
+ */
+char **split_line(char *line)
+{
+ int bufsize = 64;
+ int i = 0;
+ char **tokens = malloc(bufsize * sizeof(char *));
+ char *token;
+
+ if (!tokens)
+ {
+  fprintf(stderr, "allocation error in split_line: tokens\n");
+  exit(EXIT_FAILURE);
+ }
+ token = strtok(line, TOK_DELIM);
+ while (token != NULL)
+ {
+  /* handle comments */
+  if (token[0] == '#')
+  {
+   break;
+  }
+  tokens[i] = token;
+  i++;
+  if (i >= bufsize)
+  {
+   bufsize += bufsize;
+   tokens = realloc(tokens, bufsize * sizeof(char *));
+   if (!tokens)
+   {
+    fprintf(stderr, "reallocation error in split_line: tokens");
+    exit(EXIT_FAILURE);
+   }
+  }
+  token = strtok(NULL, TOK_DELIM);
+ }
+ tokens[i] = NULL;
+ return (tokens);
+}
+
+/**
+ * execute_args - map if command is a builtin or a process
+ * @args: command and its flags
+ *
+ * Return: 1 on sucess, 0 otherwise
+ */
+int execute_args(char **args)
+{
+ char *builtin_func_list[] = {
+  "cd",
+  "env",
+  "help",
+  "exit"
+ };
+ int (*builtin_func[])(char **) = {
+  &own_cd,
+  &own_env,
+  &own_help,
+  &own_exit
+ };
+ int i = 0;
+
+ if (args[0] == NULL)
+ {
+  /* empty command was entered */
+  return (-1);
+ }
+ /* find if the command is a builtin */
+ for (; i < sizeof(builtin_func_list) / sizeof(char *); i++)
+ {
+  /* if there is a match execute the builtin command */
+  if (strcmp(args[0], builtin_func_list[i]) == 0)
+  {
+   return ((*builtin_func[i])(args));
+  }
+ }
+ /* create a new process */
+ return (new_process(args));
+}
